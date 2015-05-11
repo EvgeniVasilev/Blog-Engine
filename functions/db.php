f7278a61370e73d297fab5b1303159aac8f15e37
EvgeniVasilev <evgenivasilev1209@abv.bg>
2015-05-11 12:39:24 +0300

    Update db.php

--- functions/db.php
+++ functions/db.php
@@ -2,7 +2,7 @@
 
 try
 {
-    $mysqli = new mysqli('localhost', 'root', '', 'blogger');
+    $mysqli = new mysqli('', '', '', '');
     if ($mysqli->connect_errno)
     {
         echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
